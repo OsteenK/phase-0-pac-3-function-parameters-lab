86639774c867a2331dd029ecbead315edbea915b
Osteen <github email address>
2022-11-18 23:13:15 +0300

    Completed assingment

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function introduction(firstName) {
+  return(`Hi, my name is ${firstName}.`);
+}
+ introduction("Aki");
+
+function introductionWithLanguage(name,language) {
+  return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+ }
+ introductionWithLanguage("Aki","React");
+
+function introductionWithLanguageOptional(name,language="JavaScript") {
+  return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+ }
+ introductionWithLanguageOptional("Gracie");
+
+ function introductionWithLanguageOptional(name,language="JavaScript") {
+  return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+ }
+  introductionWithLanguageOptional("Gracie", "Python");
